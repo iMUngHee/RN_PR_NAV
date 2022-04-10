6c4df4d92e019fec35b2ea29316ecd2686f9200b
LUH <ung7497@ajou.ac.kr>
2022-04-10 12:44:31 +0900

    init

---
+++ App.js
@@ -0,0 +1,7 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return <NavigationContainer></NavigationContainer>;
+}
